e059748af0863a50899050f2e101bfd1ad79fa33
sgopal <senthilkumar.gopal@hbo.com>
2019-11-20 16:14:06 -0500

    fixed the issue

--- main.tf
+++ main.tf
@@ -12,18 +12,18 @@ locals {
 
 resource "aws_cloudwatch_metric_alarm" "billing_alarm_to_existing_sns" {
   for_each  = local.cloudwatch_alarm
-  alarm_name          = "billing-alarm-lookup(each.value, "monthly_billing_threshold", null)"
+  alarm_name          = "${each.key}"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = "1"
   metric_name         = "EstimatedCharges"
   namespace           = "AWS/Billing"
   period              = "28800"
   statistic           = "Maximum"
-  alarm_description   = "Billing consolidated alarm >= US$ lookup(each.value, "monthly_billing_threshold", null)"
+  alarm_description   = "Billing consolidated alarm >= US$ lookup(each.value, 'monthly_billing_threshold', null)"
   threshold           = lookup(each.value, "monthly_billing_threshold", null)
   alarm_actions       = [ lookup(each.value, "aws_sns_topic_arn", null) ]
 
   dimensions = {
-    Currency = "lookup(each.value, "currency", null)"
+    Currency = lookup(each.value, "currency", null)
   }
 }
