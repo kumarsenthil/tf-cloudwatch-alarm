755ea5c0c93881391a375827cbf4d1693294ff6c
sgopal <senthilkumar.gopal@hbo.com>
2019-11-20 16:42:05 -0500

    updated

--- main.tf
+++ main.tf
@@ -7,19 +7,16 @@ locals {
   cloudwatch_alarm = var.cloudwatch_alarm
 }
 
-
-
-
 resource "aws_cloudwatch_metric_alarm" "billing_alarm_to_existing_sns" {
   for_each  = local.cloudwatch_alarm
-  alarm_name          = "${each.key}"
+  alarm_name          = each.key
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = "1"
   metric_name         = "EstimatedCharges"
   namespace           = "AWS/Billing"
-  period              = "28800"
+  period              = "21600"
   statistic           = "Maximum"
-  alarm_description   = "Billing consolidated alarm >= US$ lookup(each.value, 'monthly_billing_threshold', null)"
+  alarm_description   = "Billing consolidated alarm >= US$"
   threshold           = lookup(each.value, "monthly_billing_threshold", null)
   alarm_actions       = [ lookup(each.value, "sns_topic_arn", null) ]
 
